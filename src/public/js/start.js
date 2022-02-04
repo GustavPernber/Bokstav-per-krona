2a25f03e441589752168ada7328f88272004e975
Gustav-Pernber-nti-johanneberg <74712621+Gustav-Pernber-nti-johanneberg@users.noreply.github.com>
2022-02-04 12:51:26 +0100

    aside filter init

--- src/public/js/start.js
+++ src/public/js/start.js
@@ -1,5 +1,15 @@
-fetch('http://localhost:4567/api')
-.then((resp)=>{
-    console.log(resp);
+console.log('index')
+
+const dropHeaders=document.querySelectorAll('.dropHeader')
+const dropContainers=document.querySelectorAll('.dropContainer')
+
+dropHeaders.forEach((el, i)=>{
+    el.addEventListener('click', ()=>{
+        const dropContainer=dropContainers[i]
+        dropContainer.classList.toggle("show")
+
+        el.classList.toggle('active')
+
+    })
 })
 

586aab553d87498b1e8d14cec14a4f7ff5003764
Gustav-Pernber-nti-johanneberg <74712621+Gustav-Pernber-nti-johanneberg@users.noreply.github.com>
2022-02-24 15:34:04 +0100

    init

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8080
+const cors=require('cors')
+const path=require('path')
+
+
+
+app.use(express.json())
+// app.use(cors())
+
+
+const ApiRouter =require('./routes/api')
+app.use('/api', ApiRouter)
+
+app.use(express.static(path.join(__dirname, '../frontend')))
+
+app.get('/', (req, res)=>{
+    res.sendFile(path.join(__dirname, '../frontend', 'index.html'))
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})

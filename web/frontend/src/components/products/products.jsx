f48f83444305765192ca0fcadd0331eedd6b3e62
Gustav-Pernber-nti-johanneberg <74712621+Gustav-Pernber-nti-johanneberg@users.noreply.github.com>
2022-02-27 15:59:46 +0100

    tasteclock smokiness

--- web/frontend/src/components/products/products.jsx
+++ web/frontend/src/components/products/products.jsx
@@ -25,6 +25,8 @@ class TasteClocks extends Component {
 
 			case "TasteClockBitter":
 				return "Beska";
+			case "TasteClockSmokiness":
+				return "Rökighet";
 
 			default:
 				return `NOT KNOWN ${name}`;
@@ -206,8 +208,8 @@ class ProductsContainer extends Component {
 					: "showOrderStock=false";
 			}
 
-			let url = `http://localhost:8080/api/productsLimited?page=${pageNum}&${orderStock}&${this.state.url}`;
-			// let url=`${window.location}api/productsLimited?page=${pageNum}&${orderStock}&${this.state.url}`
+			// let url = `http://localhost:8080/api/productsLimited?page=${pageNum}&${orderStock}&${this.state.url}`;
+			let url=`${window.location}api/productsLimited?page=${pageNum}&${orderStock}&${this.state.url}`
 			console.log(url);
 			const response = await fetch(url);
 			const data = await response.json();

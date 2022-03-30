4a45db719bce340945e8198fb7f43649098701d0
Gustav-Pernber-nti-johanneberg <74712621+Gustav-Pernber-nti-johanneberg@users.noreply.github.com>
2022-03-30 14:14:19 +0200

    update

--- web/backend/server.js
+++ web/backend/server.js
@@ -8,13 +8,6 @@ const cors=require('cors')
 app.use(express.json()) 
 app.use(cors())
 
-app.use(express.static(path.join(__dirname, '/frontend')))
-
-// app.get('/', (req, res)=>{
-//     res.sendFile(path.join(__dirname, '/frontend', 'index.html'))
-// })
-
-
 const ApiRouter =require('./routes/api')
 app.use('/api', ApiRouter)
 
